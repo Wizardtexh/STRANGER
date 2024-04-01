e67f79c1b3571ab4341de72f7543f6d65e9dc4e1
Monster <164752224+Monsterbhai@users.noreply.github.com>
2024-04-02 03:36:11 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("⚡️𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦⚡️", data="help_back")
         ],
         [
-        Button.url("⚡️𝗖𝗛𝗔𝗡𝗡𝗘𝗟⚡️", "https://t.me/SHIVANSH474"),
-        Button.url("⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️", "https://t.me/MASTIWITHFRIENDSXD")
+        Button.url("⚡️𝗖𝗛𝗔𝗡𝗡𝗘𝗟⚡️", "https://t.me/TMN_NETWORK"),
+        Button.url("⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️", "https://t.me/gabba4_spam")
         ],
         [
-        Button.url("⚡️𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛-𝗫𝗗⚡️", "https://t.me/SHIVANSH39")
+        Button.url("⚡️🚬『☠︎︎ 𝗚𝗔𝗕𝗕𝗔𝗥 ✞︎ⁱˢ 𝗕 ᵃᶜ𝗞 ☠︎︎ 』⚡️", "https://t.me/ll_GaBBaR_DaDdY_ll")
         ]
         ]
 
@@ -34,11 +34,11 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**𝗛𝗘𝗬 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝗜 𝗔𝗠  [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **✦ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬 :~ [𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛](https://t.me/SHIVANSH39)**\n\n"
-        TEXT += f"» **𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗦𝗣𝗔𝗠 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `3.2`\n"
+        TEXT += f"» **✦ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬 :~ [🚬『☠︎︎ 𝗚𝗔𝗕𝗕𝗔𝗥 ✞︎ⁱˢ 𝗕 ᵃᶜ𝗞 ☠︎︎ 』](https://t.me/ll_GaBBaR_DaDdY_ll)**\n\n"
+        TEXT += f"» **🚬『☠︎︎ 𝗚𝗔𝗕𝗕𝗔𝗥 ✞︎ⁱˢ 𝗕 ᵃᶜ𝗞 ☠︎︎ 』 𝗦𝗣𝗔𝗠 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `3.2`\n"
         TEXT += f"» **𝗧𝗘𝗟𝗘𝗧𝗛𝗢𝗡 𝗩𝗘𝗥𝗦𝗜𝗢𝗡:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
-                "https://graph.org/file/c6a2ed96648fd03377dc9.jpg",
+                "https://graph.org/file/8a53ba18edf65f81eacee.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
